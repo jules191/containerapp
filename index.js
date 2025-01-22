fa9152c250a6dc0a6be48bfea4c7b79d909b8e1e
jules191 <96821291+jules191@users.noreply.github.com>
2025-01-22 07:22:03 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,61 +1,52 @@
+// Initialisierung der Werte
 let totalWeight = document.querySelector("#totalWeight");
 let amountPortions = document.querySelector("#amountPortions");
 let portionSizeLabel = document.querySelector("#portionSize");
-let inputValueWeight = parseInt(totalWeight.value);
-let inputValuePortions = parseInt(amountPortions.value);
 
-
-
-
-function Container(key, description, weight, lid, picture){
+function Container(key, description, weight, lid, picture) {
     this.key = key;
     this.description = description;
     this.weight = weight;
     this.lid = lid;
     this.picture = picture;
 }
-potBig = new Container ("potBig", "Topf, groß", 1377, true, "./images/Topf.jpg");
-potMidHigh = new Container("potMidHigh", "Topf, mittel, hoch", 993, true, "./images/Topf.jpg");
+let potBig = new Container("potBig", "Topf, groß", 1377, true, "./images/Topf.jpg");
+let potMidHigh = new Container("potMidHigh", "Topf, mittel, hoch", 993, true, "./images/Topf.jpg");
 
 let containerCollection = {
     "potBig": potBig,
     "potMidHigh": potMidHigh
 };
 
-let dropdown = document.querySelector("#containers")
+let dropdown = document.querySelector("#containers");
 
-dropdown.addEventListener("change", function(){
+dropdown.addEventListener("change", function () {
     let selectedKey = dropdown.value;
     let selectedContainer = containerCollection[selectedKey];
     
-    document.querySelector("#weight").innerHTML = document.querySelector("#weight").innerHTML + " " + selectedContainer.weight + " g";
+    document.querySelector("#weight").innerHTML = "Gewicht: " + selectedContainer.weight + " g";
 
-    if(selectedContainer.lid){
-        document.querySelector("#lid").innerHTML = document.querySelector("#lid").innerHTML + " Ja"
-    } else{
-        document.querySelector("#lid").innerHTML = document.querySelector("#lid").innerHTML + " Nein"
-    }
-})
-
-let selectedKey = dropdown.value;
-let selectedContainer = containerCollection[selectedKey];
-
-totalWeight.addEventListener("change", function(){
-    if (isNaN(Math.floor(parseFloat(totalWeight.value, 10) / parseFloat(amountPortions.value, 10)))){
-        portionSizeLabel.innerHTML = ""
-    } else if (amountPortions.value ===""){
-        portionSizeLabel.innerHTML = "Bitte Portionsgröße angeben."
+    if (selectedContainer.lid) {
+        document.querySelector("#lid").innerHTML = "Deckel: Ja";
     } else {
-        portionSizeLabel.innerHTML = "Portionsgröße: " + (selectedContainer - Math.floor(parseFloat(totalWeight.value, 10)) / parseFloat(amountPortions.value, 10)) + " g"
+        document.querySelector("#lid").innerHTML = "Deckel: Nein";
     }
-})
+});
 
-amountPortions.addEventListener("change", function(){
-    if (isNaN(Math.floor(parseFloat(totalWeight.value, 10) / parseFloat(amountPortions.value, 10)))){
-        portionSizeLabel.innerHTML = ""
-    } else if (totalWeight.value ===""){
-        portionSizeLabel.innerHTML = "Bitte Gesamtgewicht angeben."
+function updatePortionSize() {
+    let inputValueWeight = parseFloat(totalWeight.value, 10);
+    let inputValuePortions = parseFloat(amountPortions.value, 10);
+    let selectedKey = dropdown.value;
+    let selectedContainer = containerCollection[selectedKey];
+    let containerWeight = selectedContainer ? selectedContainer.weight : 0;
+
+    if (isNaN(inputValueWeight) || isNaN(inputValuePortions) || inputValuePortions <= 0) {
+        portionSizeLabel.innerHTML = "Bitte Gesamtgewicht und Portionsgröße angeben.";
     } else {
-        portionSizeLabel.innerHTML = "Portionsgröße: " + (selectedContainer - Math.floor(parseFloat(totalWeight.value, 10)) / parseFloat(amountPortions.value, 10)) + " g"
+        portionSizeLabel.innerHTML = "Portionsgröße: " + Math.floor((inputValueWeight - containerWeight) / inputValuePortions) + " g";
     }
-})
+}
+
+totalWeight.addEventListener("change", updatePortionSize);
+amountPortions.addEventListener("change", updatePortionSize);
+dropdown.addEventListener("change", updatePortionSize);
